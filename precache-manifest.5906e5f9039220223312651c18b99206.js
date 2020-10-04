245f5c9ce7964329d14c919d7863aa4bf435971f
Volodymyr Pashchenkov <volodymyr.pashchenkov@nextiva.com>
2020-10-04 17:25:29 +0300

    Updates

---
+++ precache-manifest.5906e5f9039220223312651c18b99206.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec2b4a14cde81e36e98df7145ace59bc",
+    "url": "/simple_cra/index.html"
+  },
+  {
+    "revision": "de392ebd75844d3f9828",
+    "url": "/simple_cra/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f6ddbe83187d78cc9b88",
+    "url": "/simple_cra/static/js/2.510e7389.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/simple_cra/static/js/2.510e7389.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de392ebd75844d3f9828",
+    "url": "/simple_cra/static/js/main.17ce925b.chunk.js"
+  },
+  {
+    "revision": "af9cd4e898b37c65a78d",
+    "url": "/simple_cra/static/js/runtime-main.f6ff978e.js"
+  }
+]);
